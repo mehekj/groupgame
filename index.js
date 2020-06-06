53e1599ef95cbb1c450cd96850222bf3e36cd562
Mehek Jethani <mehek.jethani@gmail.com>
2020-06-06 14:26:29 -0400

    create and join event handlers done

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ var path = require('path');
 var PORT = process.env.PORT || 3000;
 var INDEX = '/index.html';
 
-var rooms = 0;
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
@@ -16,7 +14,28 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    
+    socket.on('new game', function(data) {
+        var room = 'room-' + Math.floor(Math.random() * 10000);
+        console.log(room);
+        socket.join(room);
+        io.to(room).emit('new joined', {nickname: data.nickname, room: room});
+        console.log(io.sockets.adapter.rooms);
+    });
+
+    socket.on('join game', function(data) {
+        console.log("join game");
+        if (!(io.sockets.adapter.rooms[data.room])) {
+            socket.emit('login error', 'Invalid room code');
+        }
+        else if (io.sockets.adapter.rooms[data.room].length >= 2) {
+            socket.emit('login error', 'This room is full');
+        }
+        else {
+            socket.emit('login error', '');
+            socket.join(data.room);
+            io.to(data.room).emit('new joined', {nickname: data.nickname, room: data.room});
+        }
+    });
 });
 
 http.listen(PORT, () => {
--- public/main.js
+++ public/main.js
@@ -1,3 +1,32 @@
 $(function () {
     var socket = io();
+
+    var nickname;
+    var room;
+
+    $("#new-game").click(function() {
+        nickname = $('#nickname').val();
+        if (nickname) {
+            socket.emit('new game', {nickname: nickname});
+        }
+    });
+
+    $("#join-game").click(function() {
+        nickname = $('#nickname').val();
+        room = $('#room').val();
+
+        if (nickname && room) {
+            socket.emit('join game', {nickname: nickname, room: room});
+        }
+    });
+
+    socket.on('new joined', function(data) {
+        $('#play-screen').append('<h3>' + data.nickname + ' has joined ' + data.room);
+        $('#play-screen').show();
+        $('#join-screen').hide();
+    });
+
+    socket.on('login error', function(text) {
+        $('#login-warning').html(text);
+    });
 });
