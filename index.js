8dd55e51e42d9a580a75fe98582a8e3d6cd38d38
Mehek Jethani <mehek.jethani@gmail.com>
2020-06-09 15:18:13 -0400

    chat complete connect4 screen added

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ var path = require('path');
 var PORT = process.env.PORT || 3000;
 var INDEX = '/index.html';
 
+
+var gameSize = 2;
+var rows = 6;
+var cols = 7;
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
@@ -14,27 +19,31 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    socket.on('new game', function(data) {
+    socket.on('new game', function(nickname) {
         var room = 'room-' + Math.floor(Math.random() * 10000);
         socket.join(room);
-        io.to(room).emit('new joined', {nickname: data.nickname, room: room});
+        io.to(room).emit('new joined', nickname, room);
     });
 
-    socket.on('join game', function(data) {
-        if (!(io.sockets.adapter.rooms[data.room])) {
+    socket.on('join game', function(nickname, room) {
+        if (!(io.sockets.adapter.rooms[room])) {
             socket.emit('login error', 'Invalid room code');
         }
-        else if (io.sockets.adapter.rooms[data.room].length >= 2) {
+        else if (io.sockets.adapter.rooms[room].length >= gameSize) {
             socket.emit('login error', 'This room is full');
         }
         else {
-            socket.join(data.room);
-            io.to(data.room).emit('new joined', {nickname: data.nickname, room: data.room});
+            socket.join(room);
+            io.to(room).emit('new joined', nickname, room);
+        }
+
+        if (io.sockets.adapter.rooms[room].length == gameSize) {
+            io.to(room).emit('start game', rows, cols, gameSize);
         }
     });
 
-    socket.on('chat message', function(msg) {
-        io.to(msg.room).emit('chat message', msg);
+    socket.on('chat message', function(nickname, room, text) {
+        io.to(room).emit('chat message', nickname, text);
     });
 });
 
--- public/main.js
+++ public/main.js
@@ -7,7 +7,7 @@ $(function () {
     $("#new-game").click(function() {
         nickname = $('#nickname').val();
         if (nickname) {
-            socket.emit('new game', {nickname: nickname});
+            socket.emit('new game', nickname);
         }
     });
 
@@ -16,22 +16,24 @@ $(function () {
         room = $('#room').val();
 
         if (nickname && room) {
-            socket.emit('join game', {nickname: nickname, room: room});
+            socket.emit('join game', nickname, room);
         }
     });
 
     $('#message-form').submit(function (e) {
         e.preventDefault();
         if ($('#message-form input').val()) {
-            socket.emit('chat message', {nickname: nickname, room: room, text: $('#message-form input').val()})
+            socket.emit('chat message', nickname, room, $('#message-form input').val())
             $('#message-form input').val('');
         }
         return false;
     });
 
-    socket.on('new joined', function(data) {
-        room = data.room;
-        $('#messages').append('<li><em>' + data.nickname + ' has joined ' + room + '<em></li>');
+
+
+    socket.on('new joined', function(nickname, room) {
+        this.room = room;
+        $('#messages').append(`<li><em>${nickname} has joined ${room}<em></li>`);
         $('#play-screen').css('display', 'flex');
         $('#join-screen').hide();
     });
@@ -40,7 +42,12 @@ $(function () {
         $('#login-warning').html(text);
     });
 
-    socket.on('chat message', function(msg) {
-        $('#messages').append('<li><strong>' + msg.nickname + ':</strong> ' + msg.text);
-    })
+    socket.on('chat message', function(nickname, text) {
+        $('#messages').append(`<li><strong>${nickname}:</strong> ${text}`);
+        $('#messages').scrollTop($('#messages')[0].scrollHeight);
+    });
+
+    socket.on('start game', function(rows, cols, gameSize) {
+        $('#messages').append(`<li><em>Everyone's here! Starting game...<em></li>`);
+    });
 });
